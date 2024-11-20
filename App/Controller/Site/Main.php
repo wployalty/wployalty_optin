a24bc7032873e868b87ba880624c8a735aac8783
sabhari-krr <sabhari.a.krr@gmail.com>
2024-11-21 04:19:52 +0530

    Issue fix: Hide signup message for guest user

--- App/Controller/Site/Main.php
+++ App/Controller/Site/Main.php
@@ -71,6 +71,8 @@ class Main {
 		add_filter( 'wlr_before_adding_menu_endpoint', '__return_false' );
 		//birthday fields
 		add_filter( 'wlr_before_adding_birthday_fields', '__return_false' );
+		//hide signup message
+		add_filter( 'wlr_show_signup_message_for_guest_user', '__return_false' );
 	}
 
 	/**
